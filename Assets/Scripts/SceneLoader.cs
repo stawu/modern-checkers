faaf0138e3a5f865531ac234816aff2d9b0dd4af
Jakub Stawiski <jkstav9@gmail.com>
2021-01-19 22:42:31 +0100

    Changed match end panel visuals

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -10,6 +10,11 @@ public class SceneLoader : MonoBehaviour
         //SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
         StartCoroutine(LoadSceneAfter3Seconds(sceneName));
     }
+
+    public void LoadSceneImmediately(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
     
     private IEnumerator LoadSceneAfter3Seconds(string sceneName)
     {
